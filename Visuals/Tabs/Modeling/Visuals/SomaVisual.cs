d1e8005b6fdee26c75b89515cf3a358e304ce13a
Hepbmstl <estelapstk@gmail.com>
2026-03-29 21:50:16 +0800

    add head

--- Visuals/Tabs/Modeling/Visuals/SomaVisual.cs
+++ Visuals/Tabs/Modeling/Visuals/SomaVisual.cs
@@ -3,6 +3,25 @@ using System.Windows.Media.Media3D;
 
 namespace NeuronCAD.Visuals.Tabs.Modeling.Visuals
 {
+    /*
+     * Copyright 2026 [Hepbmstl Hepupu]
+     *
+     * Pupu NMDA / NeuronCAD
+     * A Multi-Compartment Neuron Modeling and Dynamics Analysis Platform
+     *
+     * Licensed under the Apache License, Version 2.0 (the "License");
+     * you may not use this file except in compliance with the License.
+     * You may obtain a copy of the License at
+     *
+     * http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Unless required by applicable law or agreed to in writing, software
+     * distributed under the License is distributed on an "AS IS" BASIS,
+     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+     * See the License for the specific language governing permissions and
+     * limitations under the License.
+     */
+
     /// <summary>
     /// Visual entity for the cell body (Soma), rendered as a frustum (reuses AxonVisual logic).
     /// Inherits AxonVisual and injects the "Soma" type identifier in the constructor, following the same pattern as DendVisual.
